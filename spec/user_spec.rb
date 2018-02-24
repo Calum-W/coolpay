82980564cfdfb184297f07c1ad9226be965f1764
Cal <cal7907@hotmail.co.uk>
2018-02-24 16:50:43 +0000

    Add testing for login errors
    Check if errors are raised when no api key/username is given

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,19 +8,31 @@ describe User do
 
         values = '{
             "username": "CalW",
-            "apikey": "6A597CAA3755B4B"
+            "apikey": "6A597CAA3755B4BE"
         }'
 
         headers = {
             :content_type => 'application/json'
         }
 
-        mock_site = 'https://private-anon-002603c7c9-coolpayapi.apiary-mock.com/api/login'
+        mock_site = 'https://coolpay.herokuapp.com/api/login'
 
 
         it "sets the users token if successful" do
             user.login(headers, values, mock_site)
             expect(user.token).to be_truthy
+        end
+
+        it "raises an error if not given a username" do
+          expect{
+            user.login(headers, '{ "username": nil, "apikey": "6A597CAA3755B4B" }', mock_site)
+          }.to raise_error(RestClient::BadRequest)
+        end
+
+        it "raises an error if not given an api key" do
+          expect{
+            user.login(headers, '{ "username": "CalW", "apikey": nil }', mock_site)
+          }.to raise_error(RestClient::BadRequest)
         end
     end
 end
