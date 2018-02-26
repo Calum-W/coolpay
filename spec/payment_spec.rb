95223edf69128d52756efb3dbf21c758322a9606
Cal <cal7907@hotmail.co.uk>
2018-02-26 15:36:18 +0000

    Refactor by breaking long strings into two lines

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -6,7 +6,8 @@ describe Payment do
 
   describe "#pay" do
     it "returns confirmation the payment is processing if successful" do
-      response = '{"payment": {"id": "31db334f-9ac0-42cb-804b-09b2f899d4d2", "amount": "10.5", "currency": "GBP", "recipient_id": "6e7b146e-5957-11e6-8b77-86f30ca893d3", "status": "processing"}}'
+      response = '{"payment": {"id": "31db334f-9ac0-42cb-804b-09b2f899d4d2", "amount": "10.5",' \
+        ' "currency": "GBP", "recipient_id": "6e7b146e-5957-11e6-8b77-86f30ca893d3", "status": "processing"}}'
       stub_request(:post, "https://coolpay.herokuapp.com/api/payments").
       with(
         body: "{'payment': {'amount': 10.5, 'currency': GBP, 'recipient_id': 31db334f-9ac0-42cb-804b-09b2f899d4d2}}",
@@ -21,7 +22,8 @@ describe Payment do
           }).
           to_return(:status => 200, :body => response)
       expect { payment.send_payment("mock_token") }.
-        to output("{\"payment\": {\"id\": \"31db334f-9ac0-42cb-804b-09b2f899d4d2\", \"amount\": \"10.5\", \"currency\": \"GBP\", \"recipient_id\": \"6e7b146e-5957-11e6-8b77-86f30ca893d3\", \"status\": \"processing\"}}\n").to_stdout
+        to output("{\"payment\": {\"id\": \"31db334f-9ac0-42cb-804b-09b2f899d4d2\", \"amount\": \"10.5\"," \
+          " \"currency\": \"GBP\", \"recipient_id\": \"6e7b146e-5957-11e6-8b77-86f30ca893d3\", \"status\": \"processing\"}}\n").to_stdout
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -44,7 +44,8 @@ describe User do
          }).
           to_return(:status => 200, :body => response)
       user.login("username", "apikey")
-      expect { user.list_recipients }.to output("{\"name\"=>\"Jake McFriend\", \"id\"=>\"61587f02-ca6f-4b64-b3c9-1d35985d0415\"}\n").to_stdout
+      expect { user.list_recipients }.
+        to output("{\"name\"=>\"Jake McFriend\", \"id\"=>\"61587f02-ca6f-4b64-b3c9-1d35985d0415\"}\n").to_stdout
     end
 
     it "raises an error if the user isn't logged in" do
@@ -63,7 +64,8 @@ describe User do
          }).
           to_return(:status => 200, :body => response)
       user.login("username", "apikey")
-      expect { user.search_recipients("Jake McFriend") }.to output("{\"name\"=>\"Jake McFriend\", \"id\"=>\"61587f02-ca6f-4b64-b3c9-1d35985d0415\"}\n").to_stdout
+      expect { user.search_recipients("Jake McFriend") }.
+        to output("{\"name\"=>\"Jake McFriend\", \"id\"=>\"61587f02-ca6f-4b64-b3c9-1d35985d0415\"}\n").to_stdout
     end
 
     it "raises an error if the user isn't logged in" do
@@ -79,7 +81,8 @@ describe User do
 
   describe "#list_payments" do
     it "prints a hash of payments" do
-      response = '{"payments": [{"id": "31db334f-9ac0-42cb-804b-09b2f899d4d2", "amount": "10.50", "currency": "GBP", "recipient_id": "6e7b146e-5957-11e6-8b77-86f30ca893d3", "status": "paid"}]}'
+      response = '{"payments": [{"id": "31db334f-9ac0-42cb-804b-09b2f899d4d2", "amount": "10.50",' \
+        ' "currency": "GBP", "recipient_id": "6e7b146e-5957-11e6-8b77-86f30ca893d3", "status": "paid"}]}'
       stub_request(:get, "https://coolpay.herokuapp.com/api/payments").
       with(
         headers: {
@@ -88,7 +91,8 @@ describe User do
          }).
           to_return(:status => 200, :body => response)
       user.login("username", "apikey")
-      expect { user.list_payments }.to output("{\"id\"=>\"31db334f-9ac0-42cb-804b-09b2f899d4d2\", \"amount\"=>\"10.50\", \"currency\"=>\"GBP\", \"recipient_id\"=>\"6e7b146e-5957-11e6-8b77-86f30ca893d3\", \"status\"=>\"paid\"}\n").to_stdout
+      expect { user.list_payments }.to output("{\"id\"=>\"31db334f-9ac0-42cb-804b-09b2f899d4d2\", \"amount\"=>\"10.50\"," \
+        " \"currency\"=>\"GBP\", \"recipient_id\"=>\"6e7b146e-5957-11e6-8b77-86f30ca893d3\", \"status\"=>\"paid\"}\n").to_stdout
     end
 
     it "raises an error if the user isn't logged in" do
