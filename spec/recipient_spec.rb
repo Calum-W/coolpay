41a118d12c57e6c08c7951219c6451ec894280a7
Cal <cal7907@hotmail.co.uk>
2018-02-26 17:32:51 +0000

    Remove unnecessary before each method in recipient_spec

--- spec/recipient_spec.rb
+++ spec/recipient_spec.rb
@@ -5,7 +5,7 @@ describe Recipient do
   let(:recipient) { Recipient.new('Jake McStubby') }
 
   describe "#add" do
-    before(:each) do
+    it "sets the id of the recipient" do
       response = '{"recipient": {"name": "Jake McStubby", "id": "e9a0336b-d81d-4009-9ad1-8fa1eb43418c"}}'
       stub_request(:post, "https://coolpay.herokuapp.com/api/recipients").
          with(
@@ -20,9 +20,6 @@ describe Recipient do
           'User-Agent'=>'rest-client/2.0.2 (mingw32 x86_64) ruby/2.2.6p396'
            }).
           to_return(:status => 200, :body => response)
-    end
-
-    it "sets the id of the recipient" do
       recipient.add("mock_token")
       expect(recipient.id).to eq("e9a0336b-d81d-4009-9ad1-8fa1eb43418c")
     end
